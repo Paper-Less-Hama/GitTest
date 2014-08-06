8c4605c789b15946a9f8ceea4c5c6543ef9d58dc
LandMark 浜 <hama@landmark-design.net>
2014-08-06 10:38:17 +0900

    ボタンを付けた

--- GitTest/ViewController.m
+++ GitTest/ViewController.m
@@ -17,13 +17,18 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)helloButtonDidPush:(UIButton *)sender {
+    UIAlertView* alert = [[UIAlertView alloc] initWithTitle:@"Hello!" message:@"This is my first X-Code project as Git repository" delegate:nil cancelButtonTitle:@"Good!" otherButtonTitles:nil];
+    [alert show];
 }
 
 @end
